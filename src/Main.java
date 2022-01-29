e65673e0b2581580255292900891356ce7fd50ab
Kelly Ryan <0347345@studentmail.ul.ie>
2022-01-29 21:54:05 +0000

    User can select single or multiple files to be displayed in application window. Module code, assignment name and student ID is also extracted during this process.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -8,26 +10,127 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Main extends Application {
     private static Connection conn = null;
+    Text currentSubmissionDisplay = new Text();
+    String currentModule, currentAssignment, currentStudentID;
+    Label currentModuleLabel = new Label();
+    Label currentAssignmentLabel = new Label();
+    Label currentStudentIDLabel = new Label();
+    Label submissionInfoLabel = new Label();
 
     public static void main(String[] args) {
-        dbSetup();
         //launch goes into Application and calls start()
         launch(args);
     }
 
-    public static void dbSetup() {
+    //entry point for JavaFX application
+    @Override
+    public void start(Stage stage) {
+        //initialize database
+        dbSetup();
+        //set up JavaFX stage/window
+        stage.getIcons().add(new Image("resources/images/inspector_logo.png")); //set application icon
+        stage.setMaximized(true);
+        stage.setTitle("Inspector");
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            exitApplication();
+        });
+
+        //Window menu bar
+        MenuBar menuBar = new MenuBar();
+        //File Menu
+        Menu fileMenu = new Menu("_File");
+        MenuItem openMenuItem = new MenuItem("_Open submission...");
+        FileChooser fileChooser = new FileChooser();
+        openMenuItem.setOnAction(e -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if(file != null) {
+                currentSubmissionDisplay.setText(readFile(file));
+            }
+        });
+        MenuItem openMultipleMenuItem = new MenuItem("Open _multiple submissions...");
+        openMultipleMenuItem.setOnAction(e -> {
+            List<File> fileList = fileChooser.showOpenMultipleDialog(stage);
+            if(fileList != null) {
+                StringBuilder sb = new StringBuilder();
+                for (File file : fileList) {
+                    sb.append(readFile(file)).append("\n**********EOF**********\n\n");
+                }
+                currentSubmissionDisplay.setText(sb.toString());
+            }
+        });
+
+        MenuItem exitMenuItem = new MenuItem("_Exit");
+        exitMenuItem.setOnAction(e -> exitApplication());
+        fileMenu.getItems().addAll(openMenuItem, openMultipleMenuItem, new SeparatorMenuItem(), exitMenuItem);
+        menuBar.getMenus().addAll(fileMenu);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double screenWidth = screenSize.getWidth();
+
+        //set content layout
+        HBox bottomMenu = new HBox();
+        bottomMenu.setMinHeight(50);
+        bottomMenu.setPadding(new Insets(10));
+        Label label2 = new Label();
+        Button closeButton = new Button("exit");
+        closeButton.setOnAction(e -> exitApplication());
+        bottomMenu.getChildren().addAll(label2, closeButton);
+        bottomMenu.setAlignment(Pos.CENTER);
+
+        VBox leftMenu = new VBox();
+        leftMenu.setMinWidth(screenWidth * 0.15);
+        leftMenu.setPadding(new Insets(10));
+        leftMenu.getChildren().addAll(currentModuleLabel, currentAssignmentLabel, currentStudentIDLabel);
+
+        VBox centreDisplay = new VBox();
+        centreDisplay.setMinWidth(screenWidth * 0.5);
+        centreDisplay.setPadding(new Insets(10));  //Insets(top, right, bottom, left)
+        centreDisplay.getChildren().addAll(submissionInfoLabel, currentSubmissionDisplay);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(centreDisplay);
+
+        VBox rightMenu = new VBox();
+        rightMenu.setMinWidth(screenWidth * 0.2);
+        rightMenu.setPadding(new Insets(10));
+        Label label4 = new Label("right menu");
+        rightMenu.getChildren().add(label4);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(menuBar);
+        borderPane.setBottom(bottomMenu);
+        borderPane.setLeft(leftMenu);
+        borderPane.setCenter(scrollPane);
+        borderPane.setRight(rightMenu);
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    void dbSetup() {
         try{
             conn = dbConnect();
 
@@ -86,7 +189,7 @@ public class Main extends Application {
         }
     }
 
-    static Connection dbConnect() {
+    Connection dbConnect() {
         try {
             Class.forName("org.sqlite.JDBC");
             //create database if it does not already exist and connect to it
@@ -98,27 +201,25 @@ public class Main extends Application {
         return conn;
     }
 
-    static String readFile() {
-        //submitted assignment files should be in parent directory named with student ID number
-        File file = new File("assignments/CS4023/week03/0347345/copyFile.c");
+    //submitted assignment files should be in parent directory named with student ID number
+    String readFile(File file) {
         //split directories in filepath - "\" for Windows and "/" for Unix/Mac
         String[] filepath = file.getParentFile().toString().split("[\\\\/]");
-        String moduleCode = filepath[filepath.length-3];
+        currentModule = filepath[filepath.length-3];
         //assignmentName could be a week number, e.g. labs
-        String assignmentName = filepath[filepath.length-2];
+        currentAssignment = filepath[filepath.length-2];
         //parent directory of source files named with student ID
-        String studentID = filepath[filepath.length-1];
+        currentStudentID = filepath[filepath.length-1];
 
         //DEBUG
         for(String s : filepath){
             System.out.print(s + " ");
         }
-        System.out.println("\nModule Code: " + moduleCode);
-        System.out.println("Assignment: " + assignmentName);
-        System.out.println("Student ID: " + studentID);
+        System.out.println("\nModule Code: " + currentModule);
+        System.out.println("Assignment: " + currentAssignment);
+        System.out.println("Student ID: " + currentStudentID);
 
         StringBuilder sb = new StringBuilder();
-
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -128,78 +229,18 @@ public class Main extends Application {
                 sb.append(line).append("\n");
                 line = br.readLine();
             }
-
-
-            //String fileText = sb.toString();
-            //display file in text area
-            //System.out.println(fileText);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //set labels
+        currentModuleLabel.setText("Module: " + currentModule);
+        currentAssignmentLabel.setText("Assignment: " + currentAssignment);
+        currentStudentIDLabel.setText("Student ID: " + currentStudentID);
+        submissionInfoLabel.setText(currentModule + "\t/\t" + currentAssignment + "\t/\t" + currentStudentID + "    \n\n");
+
+        //set centre pane labels
         return sb.toString();
-    }
-
-    //entry point for JavaFX application
-    @Override
-    public void start(Stage stage) {
-        stage.setMaximized(true);
-        stage.setTitle("Inspector");
-        stage.setOnCloseRequest(e -> {
-            e.consume();
-            exitApplication();
-        });
-
-        //Menu
-        Menu fileMenu = new Menu("File");
-        fileMenu.getItems().add(new MenuItem("Import Files..."));
-        fileMenu.getItems().add(new MenuItem("Export Grades..."));
-        fileMenu.getItems().add(new SeparatorMenuItem());
-        fileMenu.getItems().add(new MenuItem("Settings..."));
-        fileMenu.getItems().add(new SeparatorMenuItem());
-
-        MenuItem exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.setOnAction(e -> exitApplication());
-        fileMenu.getItems().add(exitMenuItem);
-
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
-
-        HBox bottomMenu = new HBox();
-        bottomMenu.setMinHeight(100);
-        Label label2 = new Label();
-        Button closeButton = new Button("exit");
-        closeButton.setOnAction(e -> exitApplication());
-        bottomMenu.getChildren().addAll(label2, closeButton);
-        bottomMenu.setAlignment(Pos.CENTER);
-
-        VBox leftMenu = new VBox();
-        leftMenu.setMinWidth(200);
-        Label moduleCodeLabel = new Label("CS4023");
-        Label assignmentNameLabel = new Label("week03");
-        Label studentIDLabel = new Label("0347345");
-        leftMenu.getChildren().addAll(moduleCodeLabel, assignmentNameLabel, studentIDLabel);
-
-        VBox rightMenu = new VBox();
-        rightMenu.setMinWidth(200);
-        Label label4 = new Label("right menu");
-        rightMenu.getChildren().add(label4);
-
-        VBox centreDisplay = new VBox();
-        Label displayAssignment = new Label(readFile());
-        centreDisplay.getChildren().add(displayAssignment);
-
-        BorderPane borderPane = new BorderPane();
-        //Insets(top, right, bottom, left)
-        borderPane.setPadding(new Insets(0, 10, 10, 10 ));
-        borderPane.setTop(menuBar);
-        borderPane.setBottom(bottomMenu);
-        borderPane.setLeft(leftMenu);
-        borderPane.setRight(rightMenu);
-        borderPane.setCenter(centreDisplay);
-
-        Scene scene = new Scene(borderPane);
-        stage.setScene(scene);
-        stage.show();
     }
 
     void exitApplication() {
