695cae9139aea97bd33e57b569bd7b911d17ca65
Kelly Ryan <0347345@studentmail.ul.ie>
2022-01-30 20:35:21 +0000

    Implemented TreeView to display directories and files contained therein.

--- src/Main.java
+++ src/Main.java
@@ -29,19 +29,80 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class Main extends Application {
     private static Connection conn = null;
+    //user should be able to set this at initial set up and amend it when required
+    File importDirectory = new File("C:\\Users\\mcnei\\OneDrive - University of Limerick\\CS4617 FYP\\official documents\\Inspector\\assignments");
+    Stage stage = new Stage();
     Text currentSubmissionDisplay = new Text();
+
     String currentModule, currentAssignment, currentStudentID;
     Label currentModuleLabel = new Label();
     Label currentAssignmentLabel = new Label();
     Label currentStudentIDLabel = new Label();
     Label submissionInfoLabel = new Label();
 
+    TreeView<File> treeView = new TreeView<>();
+
     public static void main(String[] args) {
         //launch goes into Application and calls start()
         launch(args);
+    }
+
+    void displayFileTree(File inputDirectoryLocation) {
+        //create root item
+        TreeItem<File> rootItem = new TreeItem<>(inputDirectoryLocation);
+        //hide root item of treeview
+        treeView.setShowRoot(false);
+        treeView.setRoot(rootItem);
+
+        //create cell factory to render tree cells
+        //treeView.setCellFactory(CheckBoxTreeCell.forTreeView());
+
+        treeView.setCellFactory(treeView -> {
+            TreeCell<File> cell = new TreeCell<>() {
+                @Override
+                public void updateItem(File file, boolean empty) {
+                    super.updateItem(file, empty) ;
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(file.getName());
+                    }
+                }
+            };
+            cell.setOnMouseClicked(event -> {
+                if (!cell.isEmpty()) {
+                    //TODO display submission on click
+                    TreeItem<File> treeItem = cell.getTreeItem();
+                    System.out.println(treeItem.getValue());
+                }
+            });
+            return cell ;
+        });
+
+        //get file list from importDirectory
+        File[] fileList = inputDirectoryLocation.listFiles();
+
+        //populate tree
+        assert fileList != null;
+        for (File file : fileList) {
+            createFileTree(file, rootItem);
+        }
+    }
+
+    void createFileTree(File file, TreeItem<File> parent) {
+        //create a new tree item with the file name and add it to parent
+        TreeItem<File> fileItem = new TreeItem<>(new File(file.getName()));
+        parent.getChildren().add(fileItem);
+        //if this file is a directory then call this method on each file inside the directory
+        if (file.isDirectory()) {
+            for (File f : Objects.requireNonNull(file.listFiles())) {
+                createFileTree(f, fileItem);
+            }
+        }
     }
 
     //entry point for JavaFX application
@@ -50,7 +111,7 @@ public class Main extends Application {
         //initialize database
         dbSetup();
         //set up JavaFX stage/window
-        stage.getIcons().add(new Image("resources/images/inspector_logo.png")); //set application icon
+            stage.getIcons().add(new Image("resources/images/inspector_logo.png")); //set application icon
         stage.setMaximized(true);
         stage.setTitle("Inspector");
         stage.setOnCloseRequest(e -> {
@@ -58,74 +119,7 @@ public class Main extends Application {
             exitApplication();
         });
 
-        //Window menu bar
-        MenuBar menuBar = new MenuBar();
-        //File Menu
-        Menu fileMenu = new Menu("_File");
-        MenuItem openMenuItem = new MenuItem("_Open submission...");
-        FileChooser fileChooser = new FileChooser();
-        openMenuItem.setOnAction(e -> {
-            File file = fileChooser.showOpenDialog(stage);
-            if(file != null) {
-                currentSubmissionDisplay.setText(readFile(file));
-            }
-        });
-        MenuItem openMultipleMenuItem = new MenuItem("Open _multiple submissions...");
-        openMultipleMenuItem.setOnAction(e -> {
-            List<File> fileList = fileChooser.showOpenMultipleDialog(stage);
-            if(fileList != null) {
-                StringBuilder sb = new StringBuilder();
-                for (File file : fileList) {
-                    sb.append(readFile(file)).append("\n**********EOF**********\n\n");
-                }
-                currentSubmissionDisplay.setText(sb.toString());
-            }
-        });
-
-        MenuItem exitMenuItem = new MenuItem("_Exit");
-        exitMenuItem.setOnAction(e -> exitApplication());
-        fileMenu.getItems().addAll(openMenuItem, openMultipleMenuItem, new SeparatorMenuItem(), exitMenuItem);
-        menuBar.getMenus().addAll(fileMenu);
-
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        double screenWidth = screenSize.getWidth();
-
-        //set content layout
-        HBox bottomMenu = new HBox();
-        bottomMenu.setMinHeight(50);
-        bottomMenu.setPadding(new Insets(10));
-        Label label2 = new Label();
-        Button closeButton = new Button("exit");
-        closeButton.setOnAction(e -> exitApplication());
-        bottomMenu.getChildren().addAll(label2, closeButton);
-        bottomMenu.setAlignment(Pos.CENTER);
-
-        VBox leftMenu = new VBox();
-        leftMenu.setMinWidth(screenWidth * 0.15);
-        leftMenu.setPadding(new Insets(10));
-        leftMenu.getChildren().addAll(currentModuleLabel, currentAssignmentLabel, currentStudentIDLabel);
-
-        VBox centreDisplay = new VBox();
-        centreDisplay.setMinWidth(screenWidth * 0.5);
-        centreDisplay.setPadding(new Insets(10));  //Insets(top, right, bottom, left)
-        centreDisplay.getChildren().addAll(submissionInfoLabel, currentSubmissionDisplay);
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setContent(centreDisplay);
-
-        VBox rightMenu = new VBox();
-        rightMenu.setMinWidth(screenWidth * 0.2);
-        rightMenu.setPadding(new Insets(10));
-        Label label4 = new Label("right menu");
-        rightMenu.getChildren().add(label4);
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(menuBar);
-        borderPane.setBottom(bottomMenu);
-        borderPane.setLeft(leftMenu);
-        borderPane.setCenter(scrollPane);
-        borderPane.setRight(rightMenu);
-
-        Scene scene = new Scene(borderPane);
+        Scene scene = new Scene(createSubmissionView());
         stage.setScene(scene);
         stage.show();
     }
@@ -201,6 +195,82 @@ public class Main extends Application {
         return conn;
     }
 
+    MenuBar createMenuBar() {
+        //Window menu bar
+        MenuBar menuBar = new MenuBar();
+        //File Menu
+        Menu fileMenu = new Menu("_File");
+        MenuItem openMenuItem = new MenuItem("_Open submission...");
+        FileChooser fileChooser = new FileChooser();
+        openMenuItem.setOnAction(e -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if(file != null) {
+                currentSubmissionDisplay.setText(readFile(file));
+            }
+        });
+        MenuItem openMultipleMenuItem = new MenuItem("Open _multiple submissions...");
+        openMultipleMenuItem.setOnAction(e -> {
+            List<File> fileList = fileChooser.showOpenMultipleDialog(stage);
+            if(fileList != null) {
+                StringBuilder sb = new StringBuilder();
+                for (File file : fileList) {
+                    sb.append(readFile(file)).append("\n**********EOF**********\n\n");
+                }
+                currentSubmissionDisplay.setText(sb.toString());
+            }
+        });
+
+        MenuItem exitMenuItem = new MenuItem("_Exit");
+        exitMenuItem.setOnAction(e -> exitApplication());
+        fileMenu.getItems().addAll(openMenuItem, openMultipleMenuItem, new SeparatorMenuItem(), exitMenuItem);
+        menuBar.getMenus().addAll(fileMenu);
+
+        return menuBar;
+    }
+
+    BorderPane createSubmissionView() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double screenWidth = screenSize.getWidth();
+
+        //set content layout
+        HBox bottomMenu = new HBox();
+        bottomMenu.setMinHeight(50);
+        bottomMenu.setPadding(new Insets(10));
+        Label label2 = new Label();
+        Button closeButton = new Button("exit");
+        closeButton.setOnAction(e -> exitApplication());
+        bottomMenu.getChildren().addAll(label2, closeButton);
+        bottomMenu.setAlignment(Pos.CENTER);
+
+        VBox leftMenu = new VBox();
+        leftMenu.setMinWidth(screenWidth * 0.15);
+        leftMenu.setPadding(new Insets(10));
+        displayFileTree(importDirectory);
+        leftMenu.getChildren().addAll(currentModuleLabel, currentAssignmentLabel, currentStudentIDLabel, treeView);
+
+        VBox centreDisplay = new VBox();
+        centreDisplay.setMinWidth(screenWidth * 0.5);
+        centreDisplay.setPadding(new Insets(10));  //Insets(top, right, bottom, left)
+        centreDisplay.getChildren().addAll(submissionInfoLabel, currentSubmissionDisplay);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(centreDisplay);
+
+        VBox rightMenu = new VBox();
+        rightMenu.setMinWidth(screenWidth * 0.2);
+        rightMenu.setPadding(new Insets(10));
+        Label label4 = new Label("right menu");
+        rightMenu.getChildren().add(label4);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(createMenuBar());
+        borderPane.setBottom(bottomMenu);
+        borderPane.setLeft(leftMenu);
+        borderPane.setCenter(scrollPane);
+        borderPane.setRight(rightMenu);
+
+        return borderPane;
+    }
+
     //submitted assignment files should be in parent directory named with student ID number
     String readFile(File file) {
         //split directories in filepath - "\" for Windows and "/" for Unix/Mac
@@ -211,14 +281,13 @@ public class Main extends Application {
         //parent directory of source files named with student ID
         currentStudentID = filepath[filepath.length-1];
 
-        //DEBUG
-        for(String s : filepath){
-            System.out.print(s + " ");
-        }
-        System.out.println("\nModule Code: " + currentModule);
-        System.out.println("Assignment: " + currentAssignment);
-        System.out.println("Student ID: " + currentStudentID);
+        //set labels
+        currentModuleLabel.setText("Module: " + currentModule);
+        currentAssignmentLabel.setText("Assignment: " + currentAssignment);
+        currentStudentIDLabel.setText("Student ID: " + currentStudentID);
+        submissionInfoLabel.setText(currentModule + "\t/\t" + currentAssignment + "\t/\t" + currentStudentID + "    \n\n");
 
+        //read file text
         StringBuilder sb = new StringBuilder();
         try {
             FileReader fr = new FileReader(file);
@@ -232,14 +301,6 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //set labels
-        currentModuleLabel.setText("Module: " + currentModule);
-        currentAssignmentLabel.setText("Assignment: " + currentAssignment);
-        currentStudentIDLabel.setText("Student ID: " + currentStudentID);
-        submissionInfoLabel.setText(currentModule + "\t/\t" + currentAssignment + "\t/\t" + currentStudentID + "    \n\n");
-
-        //set centre pane labels
         return sb.toString();
     }
 
